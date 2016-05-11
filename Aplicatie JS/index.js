058471c49835e726651de6a3b7d78a2b7083c68b
Colta Emanuela <coltaemanuela@gmail.com>
2016-05-11 19:10:33 +0300

    Add files via upload

---
+++ Aplicatie JS/index.js
@@ -0,0 +1,21 @@
+var a= document.getElementById('introdu').value;
+
+function isOdd(){
+	
+	if(typeof a != number) 
+
+		alert (a+"is not a valid value. Please write a number");
+
+	else{
+
+		if(a%2==1)
+			alert(a+"is an odd number");
+		else
+			alert(a+"is not an odd number ");
+	}
+		
+}
+
+
+
+
